b3cd6f29996fe5088f7d6a684ad13bf110a2bc61
Jenny Morgan <jennymorgna0712@gmail.com>
2023-05-28 18:59:52 -0500

    Initial Commit

---
+++ Ceyati/CeyatiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CeyatiApp.swift
+//  Ceyati
+//
+//  Created by Jenny Morgan Ryan on 5/28/23.
+//
+
+import SwiftUI
+
+@main
+struct CeyatiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
